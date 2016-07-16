8173b1aaf1fc183d2466ffee71f60e52c0c7a778
kaermessi@163.com <kaermessi@163.com>
2016-07-16 14:35:54 +0800

    demo1

---
+++ lesson1/demo1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*-coding:utf-8-*-
+'''
+@author__ = 'kaerser'
+@time:2016/7/16 14:32
+'''
+def hello():
+	print '中华人民共和国浙江省杭州市西湖区骆家庄西苑一区61号'
+
+if __name__ == '__main__':
+	hello()
