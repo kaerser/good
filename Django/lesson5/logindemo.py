c18cdbf0b91ee180463c60134bf2a97c9c094f65
kaermessi@163.com <kaermessi@163.com>
2016-09-25 23:35:40 +0800

    lesson6

--- Django/lesson5/logindemo.py
+++ Django/lesson5/logindemo.py
@@ -6,25 +6,48 @@
 """
 
 import web
-# from web import form
 
 urls = (
-    '/login' 'login',
+    '/login', 'login',
     '/index', 'index',
+	'/page1', 'page1',
+	'/page2', 'page2',
 )
 
 app = web.application(urls,globals())
 render = web.template.render('templates/')
 
+# 数据库连接
+dblink = web.database(dbn='mysql', host='localhost', user='root', pw='123456', db='test')
+
 class index(object):
 	def GET(self):
-		# name = 'World'
-		# return render.index(name)
-		return render.login()
+		return render.index()
 
 class login(object):
 	def GET(self):
 		return render.login()
+
+class page1(object):
+	def GET(self):
+		dict1 = {}
+		user = web.input().get('username')
+		pw = web.input().get('password')
+		result = dblink.select('input')
+		for i in result:
+			dict1[i.get('name')] = i.get('pass')
+		print dict1
+		if user in dict1.keys() and dict1[user] == pw:
+			return '<p>恭喜你，正常登陆系统!</p>'
+		else:
+			return '<p>你的输入有误</p>'
+
+	def POST(self):
+		return self.GET()
+
+class page2(object):
+	def GET(self):
+		return render.page2
 
 
 if __name__ == "__main__":
